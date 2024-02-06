1be9c98b96c07e17bf36d5d10f1fb84bc41ff25c
utkarshShwetam <utkarshrj3@gmail.com>
2024-02-06 17:39:15 +0530

    added unit test for db container check

--- src/test/java/com/vibrant/vibranium/vibraniumApplicationTests.java
+++ src/test/java/com/vibrant/vibranium/vibraniumApplicationTests.java
@@ -2,12 +2,27 @@ package com.vibrant.vibranium;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Testcontainers
 class vibraniumApplicationTests {
 
+	@Container
+	private static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER = new PostgreSQLContainer("postgres:latest")
+			.withDatabaseName("ushwetam-doa-unit-test")
+			.withUsername("ushwetam")
+			.withPassword("admin");
+
 	@Test
-	void contextLoads() {
+	void canStartPostgresDB() {
+		assertThat(POSTGRE_SQL_CONTAINER.isRunning()).isTrue();
+		assertThat(POSTGRE_SQL_CONTAINER.isCreated()).isTrue();
+
 	}
 
 }
