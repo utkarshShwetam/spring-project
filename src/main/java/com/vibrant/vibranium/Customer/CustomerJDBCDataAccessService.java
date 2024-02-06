3db7429f2f24cf0bd1072812a8d09764f5c91acc
utkarshShwetam <utkarshrj3@gmail.com>
2024-02-06 16:19:39 +0530

    jdbc methods implemented for update, delete and exists check

--- src/main/java/com/vibrant/vibranium/Customer/CustomerJDBCDataAccessService.java
+++ src/main/java/com/vibrant/vibranium/Customer/CustomerJDBCDataAccessService.java
@@ -3,8 +3,8 @@ package com.vibrant.vibranium.Customer;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.sql.ResultSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Repository("jdbc")
@@ -45,21 +45,44 @@ public class CustomerJDBCDataAccessService implements CustomerDAO{
 
     @Override
     public boolean existsCustomerByEmail(String email) {
-        return false;
+        String query = """
+                SELECT count(id) FROM CUSTOMER WHERE email =?""";
+       Integer result = jdbcTemplate.queryForObject(query,Integer.class,email);
+       return Objects.nonNull(result) && result > 0;
+
     }
 
     @Override
     public boolean existsCustomerById(Integer id) {
-        return false;
+        String query = """
+                SELECT count(id) FROM CUSTOMER WHERE id =?""";
+       Integer result = jdbcTemplate.queryForObject(query,Integer.class,id);
+       return Objects.nonNull(result) && result > 0;
     }
 
     @Override
     public void deleteCustomerById(Integer id) {
-
+        String query = """
+                DELETE FROM CUSTOMER WHERE id =?""";
+        jdbcTemplate.update(query,id);
     }
 
     @Override
     public void updateCustomer(Customer customerToBeUpdated) {
-
+        if(Objects.nonNull(customerToBeUpdated.getName())){
+            String query = """
+                    UPDATE CUSTOMER SET name =? WHERE id =?""";
+            jdbcTemplate.update(query,customerToBeUpdated.getName(),customerToBeUpdated.getId());
+        }
+        if(Objects.nonNull(customerToBeUpdated.getEmail())){
+            String query = """
+                    UPDATE CUSTOMER SET email =? WHERE id =?""";
+            jdbcTemplate.update(query,customerToBeUpdated.getEmail(),customerToBeUpdated.getId());
+        }
+        if(Objects.nonNull(customerToBeUpdated.getAge())){
+            String query = """
+                    UPDATE CUSTOMER SET age =? WHERE id =?""";
+            jdbcTemplate.update(query,customerToBeUpdated.getAge(),customerToBeUpdated.getId());
+        }
     }
 }
