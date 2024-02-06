ed3d7c0450e28a93ae6297dc96f6bede28c9c8aa
utkarshShwetam <utkarshrj3@gmail.com>
2024-02-06 18:01:09 +0530

    added dynamic data source fetch and flyway migration check

--- src/test/java/com/vibrant/vibranium/vibraniumApplicationTests.java
+++ src/test/java/com/vibrant/vibranium/vibraniumApplicationTests.java
@@ -1,7 +1,10 @@
 package com.vibrant.vibranium;
 
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -18,11 +21,26 @@ class vibraniumApplicationTests {
 			.withUsername("ushwetam")
 			.withPassword("admin");
 
+	@DynamicPropertySource
+	public static void registerDataSourceProperty(DynamicPropertyRegistry dynamicPropertyRegistry){
+
+		dynamicPropertyRegistry.add("spring.datasource.url", POSTGRE_SQL_CONTAINER::getJdbcUrl);
+		dynamicPropertyRegistry.add("spring.datasource.username", POSTGRE_SQL_CONTAINER::getUsername);
+		dynamicPropertyRegistry.add("spring.datasource.password", POSTGRE_SQL_CONTAINER::getPassword);
+
+	}
+
 	@Test
 	void canStartPostgresDB() {
 		assertThat(POSTGRE_SQL_CONTAINER.isRunning()).isTrue();
 		assertThat(POSTGRE_SQL_CONTAINER.isCreated()).isTrue();
 
+	}
+
+	@Test
+	void canApplyFlywayMigration() {
+		Flyway flyway = Flyway.configure().dataSource(POSTGRE_SQL_CONTAINER.getJdbcUrl(), POSTGRE_SQL_CONTAINER.getUsername(), POSTGRE_SQL_CONTAINER.getPassword()).load();
+		flyway.migrate();
 	}
 
 }
