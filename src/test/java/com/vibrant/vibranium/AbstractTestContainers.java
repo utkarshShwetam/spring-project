956df54fab82b4ad0ae246dcd92c60443a7d0f0f
utkarshShwetam <utkarshrj3@gmail.com>
2024-02-09 19:11:54 +0530

    refactored AbstractTestContainers

--- src/test/java/com/vibrant/vibranium/AbstractTestContainers.java
+++ src/test/java/com/vibrant/vibranium/AbstractTestContainers.java
@@ -1,22 +1,36 @@
 package com.vibrant.vibranium;
 
+import com.github.javafaker.Faker;
 import org.flywaydb.core.Flyway;
 import org.junit.BeforeClass;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import javax.sql.DataSource;
+
 @Testcontainers
 public abstract class AbstractTestContainers {
 
+    protected static final Faker FAKER = new Faker();
+
     @BeforeClass
-    static void beforeClass() {
+    public static void beforeClass() {
+
         Flyway flyway = Flyway.configure().dataSource(POSTGRE_SQL_CONTAINER.getJdbcUrl(), POSTGRE_SQL_CONTAINER.getUsername(), POSTGRE_SQL_CONTAINER.getPassword()).load();
         flyway.migrate();
 
     }
+
+    @Container
+    public static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER = new PostgreSQLContainer("postgres:latest")
+            .withDatabaseName("ushwetam-unit-test")
+            .withUsername("ushwetam")
+            .withPassword("admin");
 
     @DynamicPropertySource
     public static void registerDataSourceProperty(DynamicPropertyRegistry dynamicPropertyRegistry){
@@ -27,10 +41,19 @@ public abstract class AbstractTestContainers {
 
     }
 
-    @Container
-    public static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER = new PostgreSQLContainer("postgres:latest")
-            .withDatabaseName("ushwetam-doa-unit-test")
-            .withUsername("ushwetam")
-            .withPassword("admin");
+    private static DataSource getDataSourceProperty(){
+
+        return DataSourceBuilder.create()
+                .driverClassName(POSTGRE_SQL_CONTAINER.getDriverClassName())
+                .url(POSTGRE_SQL_CONTAINER.getJdbcUrl())
+                .username(POSTGRE_SQL_CONTAINER.getUsername())
+                .password(POSTGRE_SQL_CONTAINER.getPassword())
+                .build();
+
+    }
+
+    protected static JdbcTemplate getJDBCTemplate(){
+        return new JdbcTemplate(getDataSourceProperty());
+    }
 
 }
